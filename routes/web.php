5a5b686f3c78546ffa4b413dd439f237fc2b847b
janroeNabepero <janroe@nabepero.co.jp>
2022-04-25 08:58:09 +0800

    baseUrl is now tweets index homepage

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TweetController::class, 'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::post('/tweets', [TweetController::class, 'store'])->name('tweets.store');
